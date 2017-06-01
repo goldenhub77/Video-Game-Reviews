3a9d31afbce8de2935c750f28dc57ef2959e7656
goldenhub777 <sg300c@gmail.com>
2017-05-31 21:00:56 -0400

    finished basic video_games index page, set as root redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :video_games
+
+  root 'video_games#index'
 end
