794008c448ed189220cdbc1e58265db230b6e853
goldenhub777 <sg300c@gmail.com>
2017-06-03 21:21:09 -0400

    fixed video_games.js and video_games new.html.erb

--- app/assets/javascripts/video_games.js
+++ app/assets/javascripts/video_games.js
@@ -1,8 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$("#new_video_game").change(function() {
-  $form = $(this);
-  $input_value = $form.children("#video_game_rating").val();
-  $label = $form.children("#video_game_rating").siblings("label");
-  $label.text("Current Value = " + $input_value);
+
+function setRating() {
+  $input_value = $("#video_game_rating").val();
+  $label = $("#video_game_rating").siblings("label");
+  $label.text("(0 worst - 5 best) Rating: " + $input_value);
+}
+
+$(document).ready(function() {
+  setRating();
+  $("#new_video_game").on("change", setRating);
 })
